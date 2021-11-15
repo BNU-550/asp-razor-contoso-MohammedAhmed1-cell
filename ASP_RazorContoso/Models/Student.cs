2fd7bb6986383e063021e2ffaecd798add754230
Mohammed Ahmed <reazssj4@gmail.com>
2021-11-15 06:28:07 +0000

    Updated

--- ASP_RazorContoso/Models/Student.cs
+++ ASP_RazorContoso/Models/Student.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASP_RazorContoso.Models
 {
     public class Student
     {
         public int StudentID { get; set; }
+
+        [DisplayName("Last Name"), Required, StringLength(20)]
         public string LastName { get; set; }
+
+        [DisplayName("First Name"), Required, StringLength(20)]
         public string FirstName { get; set; }
+
+        [DisplayName("Enrol Date"), DataType(DataType.Date)]
         public DateTime EnrollmentDate { get; set; }
 
         // Relationships or navigation properties
         public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        public string FullName()
+        {
+            return FirstName + " " + LastName;
+        }
     }
 }
